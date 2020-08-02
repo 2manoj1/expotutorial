068f3761abc382ecbd522c677d77332c4b67e21a
Manoj Mukherjee <manojmukherjee777@gmail.com>
2020-08-02 16:45:14 +0530

    name update

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello techKnowledgeBase</Text>
+      <Text>Hello Tech KnowledgeBase</Text>
       <StatusBar style="auto" />
     </View>
   );
